49bed5ddf53744a1a49df659b30983e86abb7582
Tom <tom.benoit1@gmail.com>
2023-10-17 21:49:16 +0200

    SH script sudo perm

--- scripts/update_app.sh
+++ scripts/update_app.sh
@@ -3,16 +3,16 @@
 cd ..
 
 # Stop the container(s) using the following command:
-docker-compose down
+sudo docker compose down
 
 # Delete all containers using the following command:
-docker rmi -f gestion_satisfactory_app
-docker rmi -f postgres
+sudo docker rmi -f gestion_satisfactory_app
+sudo docker rmi -f postgres
 
 # Delete all volumes using the following command:
 #docker volume rm $(docker volume ls -q)
 
 # Restart the containers using the following command:
-docker-compose up -d --build
+sudo docker-compose up -d --build
 
 cd scripts
